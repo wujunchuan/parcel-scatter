de252574e55063f2d41b24f07655e172bbfdc66d
JohnTrump <wujunchuan2008@gmail.com>
2019-11-13 23:31:54 +0800

    version 1 done

--- src/scatter-v1/index.js
+++ src/scatter-v1/index.js
@@ -1,23 +1,18 @@
 import ScatterJS from "scatterjs-core";
-import ScatterEOS from "scatterjs-plugin-eosjs2"; // For using eosjs@beta and up
-import { Api, JsonRpc } from "eosjs"; // eosjs@beta(20) and up
+import ScatterEOS from "scatterjs-plugin-eosjs"; // For using eosjs@beta and up
+import Eos from "eosjs"; // eosjs@beta(20) and up
 import _network from "../../config/network";
 import transfer from "../../config/transfer";
 
 if (process.env.NODE_ENV !== "production") {
   import("eruda").then(module => {
-    // module.init();
+    module.init();
   });
 }
 
 const network = Object.assign({}, _network);
 
 const requiredFields = { accounts: [network] };
-
-const rpc = new JsonRpc(
-  `${network.protocol}://${network.host}:${network.port}`,
-  {}
-); // 初始化JsonPrc接口
 
 // Don't forget to tell ScatterJS which plugins you are using.
 ScatterJS.plugins(new ScatterEOS());
@@ -34,7 +29,7 @@ let _account = null;
 scatter.connect("scatter-v2").then(async function(connected) {
   // no connect
   if (!connected) return false;
-  _eos = scatter.eos(network, Api, { rpc, beta3: true });
+  _eos = scatter.eos(network, Eos, {});
   await getIdentity();
   // connect success
   window.ScatterJS = null;
@@ -48,7 +43,6 @@ function getIdentity() {
     await scatter.getIdentity(requiredFields);
     const account = scatter.identity.accounts.find(x => x.blockchain === "eos");
     resolve(account);
-    console.info(account);
     _account = account;
   });
 }
@@ -71,7 +65,7 @@ function transaction() {
     transfer.actions[0].data.from = _account.name;
     transfer.actions[0].authorization[0].permission = _account.authority;
     try {
-      let resp = await _eos.transact(
+      let resp = await _eos.transaction(
         {
           actions: transfer.actions
         },
