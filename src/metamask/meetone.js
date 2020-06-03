58619809df2937a0e2af1d5e4a9a0f99050dcf86
JohnTrump <wujunchuan2008@gmail.com>
2020-06-03 10:49:32 +0800

    拆分页面

---
+++ src/metamask/meetone.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: John Trump
+ * @Date: 2020-06-03 10:34:22
+ * @LastEditors: John Trump
+ * @LastEditTime: 2020-06-03 10:42:09
+ * @FilePath: /src/metamask/meetone.js
+ */
+
+import { assert } from "chai";
+import './index';
